85813276ba32064729a3cf4f9c32527b3c15e922
nicode27 <enistyrin@gmail.com>
2025-07-28 20:38:15 +0700

    menambah karir & hasil prediksi

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,20 @@
 import Link from 'next/link';
 import styles from './Header.module.css';
-<<<<<<< HEAD
-import { Bell, CircleUserRound } from 'lucide-react';
-
-interface HeaderProps {
-    isLoggedIn: boolean; 
-=======
 import { Bell, CircleUserRound, ChevronDown } from 'lucide-react';
 
 
 interface HeaderProps {
     isLoggedIn: boolean;
->>>>>>> 1036686 (feat: add header and footer)
     pageName: string; // Nama halaman untuk menentukan link aktif
     userName?: string; // Optional, hanya ada jika pengguna sudah login
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1036686 (feat: add header and footer)
 export default function Header({ isLoggedIn, pageName, userName }: HeaderProps) {
   return (
     <div className={styles.header}>
         <div className={styles.left_container}>
-<<<<<<< HEAD
-            <img src="/Logo_bg_biru.png" alt="Pathera Logo" className={styles.logo} />
-            
-=======
             <img src="/white-logo.png" alt="Pathera Logo" className={styles.logo}/>
            
->>>>>>> 1036686 (feat: add header and footer)
             <Link href="/" className={`styles.nav ${pageName === 'home' ? styles.nav_active : styles.nav}`}>
                 Beranda
             </Link>
@@ -48,24 +32,10 @@ export default function Header({ isLoggedIn, pageName, userName }: HeaderProps) 
             </Link>
         </div>
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1036686 (feat: add header and footer)
         <div className={styles.right_container}>
             {isLoggedIn ? (
                 <>  
                     <div className={styles.right_container}>
-<<<<<<< HEAD
-                        <Link href="/notification">
-                            <Bell size={32} fill="white"/>
-                        </Link>
-                        <Link href="/profile">
-                            <CircleUserRound size={32}/>
-                        </Link>
-                        <Link href="/profile" className="text-[20px]">
-                            {userName}
-=======
                         {/*To-do: Add dropdown menu for notifications */}
                         <Link href="/notification">
                             <Bell size={25} fill="white"/>
@@ -77,19 +47,11 @@ export default function Header({ isLoggedIn, pageName, userName }: HeaderProps) 
                         <Link href="/profile" className={styles.username_display}>
                             {userName}
                             <ChevronDown size={25} />
->>>>>>> 1036686 (feat: add header and footer)
                         </Link>
                     </div>
                 </>
             ) : (
                 <>
-<<<<<<< HEAD
-                    <Link href="/login" className={styles.login_link}>
-                        Login
-                    </Link>
-                    <Link href="/signup" className={styles.signup_link}>
-                        Sign Up
-=======
                     
                     <Link href="/login" className={styles.login_link}>
                         <div className={styles.login_button}>
@@ -100,15 +62,10 @@ export default function Header({ isLoggedIn, pageName, userName }: HeaderProps) 
                         <div className={styles.signup_button}>
                             Sign Up
                         </div>
->>>>>>> 1036686 (feat: add header and footer)
                     </Link>
                 </>
             )}
         </div>
     </div>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1036686 (feat: add header and footer)
