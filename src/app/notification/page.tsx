b01f0bad2b6464479796f1b29c43b4151bbffe49
kaynaa <kayna.mufidah@gmail.com>
2025-07-31 12:20:22 +0700

    fill in empty pages to deploy

---
+++ src/app/notification/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function notifPage(){
+    return(
+        <div>Notification Page</div>
+    );
+}
---
+++ src/app/training-certification/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function trainingPage(){
+  return(
+      <div>Training Page</div>
+  );
+}
